c93d0a6282e3dab9ac3295e497c0bee50ca5a1f9
adrianhuebner <adrianh92@gmail.com>
2020-07-03 23:21:07 -0700

    java code has been added

--- code/SlidingWindow/SmallestSubarrayGivenSum/SmallestSubarrayGivenSum.java
+++ code/SlidingWindow/SmallestSubarrayGivenSum/SmallestSubarrayGivenSum.java
@@ -1,5 +1,23 @@
 package code.SlidingWindow.SmallestSubarrayGivenSum;
 
 public class SmallestSubarrayGivenSum {
-    
+    public int smallSubArrayLength(int s, int[] array){
+        int minimumLength = Integer.MAX_VALUE;
+        int right = 0;
+        int currentSum = 0;
+
+        if(array.length == 0) return 0;
+
+        // did js with while and java with for loop to change it up
+        
+        for(int left = 0; left < array.length; left++){
+            currentSum += array[left];
+            while(currentSum >= s){
+                minimumLength = Math.min(minimumLength, left - right + 1);
+                currentSum -= array[left];
+                left++;
+            }
+        }
+        return minimumLength;
+    }
 }
