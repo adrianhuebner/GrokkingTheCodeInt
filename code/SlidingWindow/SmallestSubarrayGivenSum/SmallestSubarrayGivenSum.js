413df3aba4c6abf55a95ded37fa5bfd1662c3ba2
adrianhuebner <adrianh92@gmail.com>
2020-07-03 23:09:36 -0700

    javascript code written

--- code/SlidingWindow/SmallestSubarrayGivenSum/SmallestSubarrayGivenSum.js
+++ code/SlidingWindow/SmallestSubarrayGivenSum/SmallestSubarrayGivenSum.js
@@ -3,9 +3,20 @@ const smallestSubarrayGivenSum = function(s, array){
     if(!array.length) return 0;
 
     // instantiating all the variables needed 
-    let left = 0;
     let right = 0;
+    let left = 0;
     // using the global property of Infinity
     let minimumSize = Infinity;
     let currentSum = 0;
+
+    while (right != array.length){
+        currentSum += array[right];
+        while (currentSum >= s){
+            minimumSize = Math.min(minimumSize, right - left + 1);
+            currentSum -= array[left];
+            left ++;
+        }
+        right ++;
+    }
+    return minimumSize;
 }
